fbac9022071f4a274778eac83feca7a6be86ef4f
omorowange isaac <suntimeuganda2019@gmail.com>
2025-09-03 10:38:25 +0300

    more project

--- alx-react-app-props/src/App.js
+++ alx-react-app-props/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+// Create a context for user data
+const UserContext = createContext();
+
+export default UserContext;
