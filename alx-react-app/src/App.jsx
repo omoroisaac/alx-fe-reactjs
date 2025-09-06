4360d7ca8d9c59c8530ab47cde66314041888e7e
omorowange isaac <suntimeuganda2019@gmail.com>
2025-09-06 13:17:10 +0300

    React_app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 
 function App() {
   return (
