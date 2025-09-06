4867e03c3f9c69f0d1245e33045c304ed72ed5d8
omorowange isaac <suntimeuganda2019@gmail.com>
2025-09-06 13:00:10 +0300

    React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
 function App() {
   const [count, setCount] = useState(0)
 
