a60021a00ae83dfcd25cbe823a1fce586ba20204
omorowange isaac <suntimeuganda2019@gmail.com>
2025-09-06 12:42:30 +0300

    update for app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,7 +8,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+
     <>
+    <div>
+      <WelcomeMessage />
+    </div>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
