a182871b563bfe6146fb72cba7dda41f704fc99a
omorowange isaac <suntimeuganda2019@gmail.com>
2025-09-09 21:08:54 +0300

    update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,13 @@
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div style={{ padding: '20px', fontFamily: 'Arial' }}>
-      <h1>Recipe Store with Zustand</h1>
+      <h1>Recipe Store with Search & Filtering</h1>
       <AddRecipeForm />
+      <SearchBar />
       <RecipeList />
     </div>
   );
