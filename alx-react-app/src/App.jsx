fca54fc24f3ae5e99faa4f46bd1da24a0468472e
omorowange isaac <suntimeuganda2019@gmail.com>
2025-09-26 20:27:11 +0300

    User Profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,8 @@
 import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
-
+import UserProfile from "./components/UserProfile";
+<UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 function App() {
   return (
     <div>
