cc8c51d35f77cfac129ea213aa1a2234d84f047a
omorowange isaac <suntimeuganda2019@gmail.com>
2025-09-26 20:38:49 +0300

    UserProfileUpdate

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,14 @@
 import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
-import UserProfile from "./components/UserProfile";
-<UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+import UserProfile from './components/UserProfile';
+
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </div>
   );
--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,13 @@
-function UserProfile(props) {
+// src/components/UserProfile.jsx
+
+const UserProfile = (props) => {
   return (
     <div>
       <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
-      <p>{props.bio}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
-}
+};
 
 export default UserProfile;
