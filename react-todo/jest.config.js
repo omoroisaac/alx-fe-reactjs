07b1214a16da3d1361e9bdd6858aedbbd21105d5
omorowange isaac <suntimeuganda2019@gmail.com>
2025-10-13 21:24:44 +0300

    updates

---
+++ react-todo/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapping: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/index.js',
+    '!src/reportWebVitals.js'
+  ],
+};
