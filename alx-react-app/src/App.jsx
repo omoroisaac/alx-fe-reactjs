59afc58e15057e78e21ab6ce31d166e146271460
omorowange isaac <suntimeuganda2019@gmail.com>
2025-10-23 00:50:55 +0300

    Updates

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,8 @@ import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
 import UserProfile from './components/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <WelcomeMessage />
       <Footer />
     </div>
   );
